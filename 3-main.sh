377ded197c38a98a6cab5b495518ea012513fc62
Samuel Born <samuelborn@outlook.de>
2025-09-18 21:17:42 +0200

    clean main skript

--- 3-main.sh
+++ 3-main.sh
@@ -1,70 +1,26 @@
 # No password sudo
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
 
-# Screenshot script to fix launch flameshot
-screenshot_script="$HOME/.local/bin/flameshot.sh"
-cat <<EOF >$screenshot_script
-#!/bin/sh
-flameshot gui
-EOF
-chmod +x $screenshot_script
-
-# Custom Shortcuts
-gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys calculator "['<Super>c']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>f']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>b']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys help "[]"
-gsettings set org.gnome.settings-daemon.plugins.media-keys suspend "['<Super>F1']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/power-off/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/ name 'Launch Terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/ command 'kitty'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/ binding '<Super>Return'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/power-off/ name 'Power off'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/power-off/ command 'shutdown now'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/power-off/ binding '<Super>Delete'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot/ name 'Take Screenshot'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot/ command "$screenshot_script"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot/ binding 'Print'
-gsettings set org.gnome.shell.keybindings show-screenshot-ui '[]'
-
 # Install dotfiles
 cd ~/Repos/dotfiles/home
 stow --target=$HOME .
 
-# Nerd Font
-mkdir -p ~/.local/share/fonts/JetBrainsMono
-curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz | tar --extract --xz --directory ~/.local/share/fonts/JetBrainsMono
-fc-cache -f
-gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font 11'
-
 # Change default shell to zsh
 chsh --shell /bin/zsh
-
-# Disable alert bell
-gsettings set org.gnome.desktop.wm.preferences audible-bell false
-
-# Disable logout prompt
-gsettings set org.gnome.SessionManager logout-prompt false
-
-# Theme GTK 3 apps
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
 # Automatically delete trash and temporary files
 gsettings set org.gnome.desktop.privacy remove-old-trash-files true
 gsettings set org.gnome.desktop.privacy remove-old-temp-files true
 
 # GitHub add ssh key and default sign commits
-read -p "Please enter your GitHub email address: " email
+read -p "GitHub email address: " email
+read -p "Name: " name
 ssh-keygen -t ed25519 -C "$email"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
+cat ~/.ssh/id_ed25519.pub | wl-copy
 xdg-open "https://github.com/settings/keys"
-
-# Git setup with signing
-git config --global user.name "$(getent passwd $USER | cut -d ':' -f 5)"
+git config --global user.name "$name"
 git config --global user.email "$email"
 git config --global core.editor nvim
 git config --global gpg.format ssh
