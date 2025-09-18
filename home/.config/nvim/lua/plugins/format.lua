7e811291095e4ddfd749b4cec416f3d157e3b312
Samuel Born <samuelborn@outlook.de>
2025-09-18 20:26:38 +0200

    add formatter for jsonc

--- home/.config/nvim/lua/plugins/format.lua
+++ home/.config/nvim/lua/plugins/format.lua
@@ -5,6 +5,7 @@ return {
         formatters_by_ft = {
             javascript = { "prettierd" },
             json       = { "prettierd" },
+            jsonc      = { "prettierd" },
             markdown   = { "prettierd" },
             html       = { "prettierd" },
             css        = { "prettierd" },
