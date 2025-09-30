dd7f285250a63b73c5c790716b8323cf72611b0d
Samuel Born <samuelborn@outlook.de>
2025-10-01 01:28:48 +0200

    add uefi to fuzzel

--- home/.config/fuzzel/fuzzel-power-menu.sh
+++ home/.config/fuzzel/fuzzel-power-menu.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-opt=$(printf "Logout\nReboot\nShutdown\nSuspend" | fuzzel --dmenu --lines 4)
+opt=$(printf "Logout\nReboot\nShutdown\nSuspend\nUEFI" | fuzzel --dmenu --lines 5 --anchor bottom-left --hide-prompt)
 case "$opt" in
   Logout) hyprctl dispatch exit;;
   Reboot) systemctl reboot;;
   Shutdown) systemctl poweroff;;
   Suspend) systemctl suspend;;
+  UEFI) systemctl reboot --firmware;;
 esac
