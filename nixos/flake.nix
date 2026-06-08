e04e6cc23ac8723d399d69255511029a7db9c0c2
Samuel Born <58636569+samuelborn@users.noreply.github.com>
2026-06-08 16:50:53 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,6 +2,8 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { nixpkgs, ... }: {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
+
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       modules = [ ./hosts/desktop ];
     };
