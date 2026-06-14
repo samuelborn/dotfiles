b8ae8dd5fd3b9c43b4ef7306099c53dc16d77c75
Samuel Born <samuelborn@outlook.de>
2026-06-14 22:10:45 +0200

    remove unnecessary locale settings

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -24,17 +24,6 @@
   # --- Locale ---
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_DK.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_DK.UTF-8";
-    LC_IDENTIFICATION = "en_DK.UTF-8";
-    LC_MEASUREMENT = "en_DK.UTF-8";
-    LC_MONETARY = "en_DK.UTF-8";
-    LC_NAME = "en_DK.UTF-8";
-    LC_NUMERIC = "en_DK.UTF-8";
-    LC_PAPER = "en_DK.UTF-8";
-    LC_TELEPHONE = "en_DK.UTF-8";
-    LC_TIME = "en_DK.UTF-8";
-  };
 
   # --- Users ---
   users.users.born = {
